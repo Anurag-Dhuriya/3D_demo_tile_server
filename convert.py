6ace6721608fd4c0b48ae5470bb3fc001d0d09a9
Anurag Dhuriya <anuragdhuriya@Anurags-MacBook-Air.local>
2026-03-03 19:26:27 +0530

    Improved GLTF conversion and Cesium lighting adjustments

--- convert.py
+++ convert.py
@@ -7,8 +7,12 @@ def convert_obj_to_glb(obj_path):
         print("File not found!")
         return None
 
+    # Load object (allow scene or mesh)
     mesh = trimesh.load(obj_path)
+
     output_path = "static/model.glb"
+
     mesh.export(output_path)
+
     print("Converted to GLB successfully!")
     return output_path
